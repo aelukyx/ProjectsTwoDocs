9ef78e30ef60c3fcb43b506ad721bbeb6c10c375
Alexander Raúl Cabrera Guevara <arcabrerag@outlook.com>
2015-10-10 19:53:12 -0500

    Sistratur Vs.02
    Agregado el Index de Home :paperclip:  listo para la programación.

---
+++ SistemaSistraturMVC/Sistratur.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistratur.WebApp.Startup))]
+namespace Sistratur.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
